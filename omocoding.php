ed9f7f8870ca8804efbc5a603ba6a2a1c089934e
omoifoimobhio <81294694+omoifoimobhio@users.noreply.github.com>
2021-06-25 21:36:22 +0100

    Update omocoding.php

--- omocoding.php
+++ omocoding.php
@@ -1,7 +1,8 @@
 <!-- Two arguments namely start and end showing a range of numbers in a return of arrays from start to end (3,8) -->
 <?php
-$startend = range(3,8);
-print '<pre>';
-print_r($startend);
-print '</pre>';
+function startend($start, $end) {
+return range ($start, $end);
+}
+$range = startend(3,8);
+print_r($range);
 ?>
