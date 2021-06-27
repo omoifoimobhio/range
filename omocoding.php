42c18e45f33332f2b71479b4a40595fea82b86a1
omoifoimobhio <81294694+omoifoimobhio@users.noreply.github.com>
2021-06-27 03:01:40 -0700

    Update omocoding.php

--- omocoding.php
+++ omocoding.php
@@ -1,8 +1,10 @@
 <!-- Two arguments namely start and end showing a range of numbers in a return of arrays from start to end (3,8) -->
 <?php
+//Defining Function
 function startend($start, $end) {
 return range ($start, $end);
 }
+//Calling Function
 $range = startend(3,8);
 print_r($range);
 ?>
@@ -11,10 +13,11 @@ print_r($range);
 <!-- The return of the sum of numbers in the array (5,20,45,25,65) -->
 
 <?php
-
+//Defining Function
 function addNumbers($a, $b, $c, $d, $e) {
   return $a + $b + $c + $d + $e;
 }
+//Calling Function
 echo addNumbers(5,20,45,25,65);
 
 ?>
